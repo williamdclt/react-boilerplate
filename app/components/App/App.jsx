378ca178e8c8ea50e27fb0c9600be0cf8ef310d5
William Duclot <william.duclot@gmail.com>
2017-11-30 17:24:00 +0000

    Create 'pages' folder

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -8,8 +8,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'components/HomePage';
-import NotFoundPage from 'components/NotFoundPage';
+import { HomePage, NotFoundPage } from 'components/pages';
 
 const App = () => (
   <div>
